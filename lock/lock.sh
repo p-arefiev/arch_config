1ee974b019574a394f565b4f41f55b20c0e921cb
Pierre AREFIEV <pierre.arefiev@non.se.com>
2021-07-21 21:24:54 +0200

    Updated lock.sh

--- lock/lock.sh
+++ lock/lock.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Stop dunst
-pkill -u $USER -USR1 dunst
+# pkill -u $USER -USR1 dunst
 
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 5% -blur 0x4 -resize 2000% /tmp/screen.png
 
-if [[ -f $HOME/Images/lock.png ]]
+if [[ -f $HOME/lock/lock.png ]]
 then
-	convert /tmp/screen.png $HOME/Images/lock.png -composite /tmp/screen.png
+	convert /tmp/screen.png $HOME/lock/lock.png -composite /tmp/screen.png
 fi
 
 i3lock -e -n -i /tmp/screen.png \
-	--insidevercolor=2e2e2e6f --insidewrongcolor=ff79687d --insidecolor=ffffff00 \
-	--ringvercolor=b9b4aa8f --ringwrongcolor=cd89688f --ringcolor=ffffff00 \
-	--line-uses-ring --keyhlcolor=c9c4baff --bshlcolor=ff7968ff \
-	--separatorcolor=2e2e2edf --verifcolor=efefefff --wrongcolor=efefefff \
-	--veriftext="verifying" --wrongtext="incorrect" --noinputtext="none"
+	--insidever-color=2e2e2e6f --insidewrong-color=ff79687d --inside-color=ffffff00 \
+	--ringver-color=b9b4aa8f --ringwrong-color=cd89688f --ring-color=ffffff00 \
+	--line-uses-ring --keyhl-color=c9c4baff --bshl-color=ff7968ff \
+	--separator-color=2e2e2edf --verif-color=efefefff --wrong-color=efefefff \
+	--verif-text="verifying" --wrong-text="incorrect" --noinput-text="none"
 
-pkill -u $USER -USR2 dunst
+# pkill -u $USER -USR2 dunst
 
 rm /tmp/screen.png
