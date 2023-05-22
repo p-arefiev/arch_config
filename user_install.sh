294c1c2b231f647bd203fa6332718b0fb3012471
FuXE <73062901+p-arefiev@users.noreply.github.com>
2023-05-22 16:25:09 +0200

    Update user_install.sh

--- user_install.sh
+++ user_install.sh
@@ -10,8 +10,64 @@ rm -rf yay
 # User pacman installs
 sudo pacman -S network-manager-applet
 
-# Yay installs
-yay -S nerd-fonts-git i3-scrot autojump 
+# Yay nerd fonts install
+yay -S otf-aurulent-nerd \
+  otf-codenewroman-nerd \
+  otf-comicshanns-nerd \
+  otf-droid-nerd \
+  otf-firamono-nerd \
+  otf-hasklig-nerd \
+  otf-hermit-nerd \
+  otf-opendyslexic-nerd \
+  otf-overpass-nerd \
+  ttf-3270-nerd \
+  ttf-agave-nerd \
+  ttf-anonymouspro-nerd \
+  ttf-arimo-nerd \
+  ttf-bigblueterminal-nerd \
+  ttf-bitstream-vera-mono-nerd \
+  ttf-cascadia-code-nerd \
+  ttf-cousine-nerd \
+  ttf-daddytime-mono-nerd \
+  ttf-dejavu-nerd \
+  ttf-fantasque-nerd \
+  ttf-firacode-nerd \
+  ttf-go-nerd \
+  ttf-hack-nerd \
+  ttf-heavydata-nerd \
+  ttf-iawriter-nerd \
+  ttf-ibmplex-mono-nerd \
+  ttf-inconsolata-go-nerd \
+  ttf-inconsolata-lgc-nerd \
+  ttf-inconsolata-nerd \
+  ttf-iosevka-nerd \
+  ttf-iosevkaterm-nerd \
+  ttf-jetbrains-mono-nerd \
+  ttf-lekton-nerd \
+  ttf-liberation-mono-nerd \
+  ttf-lilex-nerd \
+  ttf-meslo-nerd \
+  ttf-monofur-nerd \
+  ttf-monoid-nerd \
+  ttf-mononoki-nerd \
+  ttf-mplus-nerd \
+  ttf-nerd-fonts-symbols \
+  ttf-nerd-fonts-symbols-mono \
+  ttf-noto-nerd \
+  ttf-profont-nerd \
+  ttf-proggyclean-nerd \
+  ttf-roboto-mono-nerd \
+  ttf-sharetech-mono-nerd \
+  ttf-sourcecodepro-nerd \
+  ttf-space-mono-nerd \
+  ttf-terminus-nerd \
+  ttf-tinos-nerd \
+  ttf-ubuntu-mono-nerd \
+  ttf-ubuntu-nerd \
+  ttf-victor-mono-nerd
+ 
+# Yay install depedencies
+yay -S xclip i3-scrot autojump 
 echo 'Installing oh my zsh plugins ... '
 yay -S zsh-syntax-highlighting zsh-autosuggestions 
 
