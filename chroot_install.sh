e876760108049a746ec3f2d6113ecabb8ef7f64d
FuXE <73062901+p-arefiev@users.noreply.github.com>
2023-05-22 14:05:36 +0200

    Update wip fiw script

--- chroot_install.sh
+++ chroot_install.sh
@@ -13,11 +13,13 @@ fi
 
 # setup timezone
 echo -n 'Setting up timezone ... '
-timedatectl set-ntp true > /dev/null
-ln -s /usr/share/zoneinfo/Europe/Paris /etc/localtime > /dev/null
-timedatectl set-timezone Europe/Paris > /dev/null
-hwclock --systohc > /dev/null
+timedatectl set-ntp true 
+ln -s /usr/share/zoneinfo/Europe/Paris /etc/localtime 
+timedatectl set-timezone Europe/Paris 
+hwclock --systohc
 echo 'done'
+
+read -p "Press enter to continue"
 
 # setup locale
 echo -n 'Setting up locale ... '
@@ -27,15 +29,21 @@ echo 'LANG=fr_FR.UTF-8' > /etc/locale.conf
 echo KEYMAP=fr > /etc/vconsole.conf
 echo 'done'
 
+read -p "Press enter to continue"
+
 # setup hostname
 echo -n 'Setting up hostname ... '
 echo 'arch-i3' > /etc/hostname
 echo 'done'
 
+read -p "Press enter to continue"
+
 # build
 echo 'Starting mkinitcpio ...'
 mkinitcpio -p linux > /dev/null
 echo 'done'
+
+read -p "Press enter to continue"
 
 # install bootloader
 echo 'Installing bootloader ... '
@@ -44,36 +52,47 @@ grub-install --target=i386-pc /dev/sda > /dev/null
 grub-mkconfig -o /boot/grub/grub.cfg > /dev/null
 echo 'done'
 
+read -p "Press enter to continue"
+
 # install Xorg
 echo 'Installing i3 (xorg, i3gaps, lightdm) ... '
-pacman -S --noconfirm xorg xorg-xinit xorg-apps i3-gaps i3blocks lightdm lightdm-slick-greeter > /dev/null
+pacman -S --noconfirm xorg xorg-xinit xorg-apps i3-gaps i3blocks lightdm lightdm-slick-greeter 
 echo 'done'
+
+read -p "Press enter to continue"
 
 # install virtualbox guest modules
 echo -n 'Installing virtualbox modules ... '
-pacman -S --noconfirm virtualbox-host-modules-arch virtualbox-guest-utils > /dev/null
+pacman -S --noconfirm virtualbox-host-modules-arch virtualbox-guest-utils
 # Vbox services start
 sudo systemctl enable vboxservice.service
-# sudo systemctl start vboxservice.service
+sudo systemctl start vboxservice.service
 echo 'done'
+
+read -p "Press enter to continue"
 
 # install dev envt.
 echo -n 'Installing dev env ... '
 pacman -S --noconfirm git npm go go-tools > /dev/null
 echo 'done'
 
+read -p "Press enter to continue"
+
 echo -n 'Installing text editors ... '
-pacman -S --noconfirm emacs nano zathura > /dev/null
+pacman -S --noconfirm emacs nano vi > /dev/null
 echo 'done'
 
 echo -n 'Installing sys tools ... '
 pacman -S --noconfirm rxvt-unicode zsh wget openssh numlockx nitrogen dhclient dmenu xautolock lsd bat dhcpcd
 echo 'done'
 
+read -p "Press enter to continue"
+
+# Initialisation dhcpcd
 systemctl enable dhcpcd
 
 echo -n 'Installing fonts ... '
-pacman -S --noconfirm ttf-hack > /dev/null
+pacman -S --noconfirm ttf-hack 
 echo 'done'
 
 # user creation
